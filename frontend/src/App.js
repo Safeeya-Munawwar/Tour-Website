7d987cbe22e05d6a7eb77633a1b9fd391d05dd5e
Safeeya Munawwar <shafiyasha0036@gmail.com>
2025-11-21 11:18:17 +0530

    Tour Website Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind Working!</h1>
+    </div>
+  );
+}
+
+export default App;
