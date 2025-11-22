3aa127494838a5b78d219ccd2781ae0b871b18c3
Safeeya Munawwar <shafiyasha0036@gmail.com>
2025-11-22 11:59:46 +0530

    Footer, About, & Contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            {/* Add more routes here for Packages, Destinations, etc. */}
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
