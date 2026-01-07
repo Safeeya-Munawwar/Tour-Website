7e1f20421c81f279722e66c6c2ad005ce4c6cbe8
Swetha Pathirana <swethapathirana2002@gmail.com>
2026-01-07 15:32:41 +0530

    ys

--- frontend/src/pages/Destination.jsx
+++ frontend/src/pages/Destination.jsx
@@ -56,7 +56,7 @@ export default function Destination() {
       <div className="w-full h-[360px] md:h-[560px] relative flex items-center justify-center text-white">
         {/* Hero Image */}
         <img
-          src="/images/ds.jpg"
+          src="/images/deso.webp"
           alt="Explore Sri Lanka Destinations"
           className="absolute inset-0 w-full h-full object-cover"
           width={1920}
